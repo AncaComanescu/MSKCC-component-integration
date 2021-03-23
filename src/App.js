e56829721a8013e4e2a6c3b3a0d24481714f319c
AncaComanescu <80053458+AncaComanescu@users.noreply.github.com>
2021-03-23 11:24:50 -0400

    feat(home): use home component in app.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import BlogPost from "./pages/blogPost";
 import Home from "./pages/home";
 import '@knapsack-cloud/msk-design-system/dist/main.css';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
+ 
     <Router>
-      <Route path="/doctor-bio" component={DoctorBio}></Route>
-      <Route path="/blog-post" component={BlogPost}></Route>
+        <Switch>
+          <Route path="/doctor-bio">
+            <DoctorBio />
+          </Route>
+          <Route path="/blog-post">
+            <BlogPost />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
     </Router>
-
   );
 }
 
