4f0111a702a1194caa330b82c13f07ba2e757e65
AncaComanescu <80053458+AncaComanescu@users.noreply.github.com>
2021-04-02 13:16:23 -0400

    the header sits outside the msk site wrapper component (#3)
    * the header sits outside the msk site wrapper component
    
    * remove extra div element

--- src/App.js
+++ src/App.js
@@ -11,35 +11,33 @@ import '@knapsack-cloud/msk-design-system/dist/main.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  let footerLinks=[
+  let footerLinks = [
     { text: 'Example', href: 'https://example.com' },
     { text: 'Hello, world', href: 'https://helloworld.com' },
     { text: 'FooBar', href: 'https://foobar.com' },
   ];
   return (
-    <MskSiteWrapper>
+    <div>
       <div className="msk-site-header">
         placeholder for msk site header
       </div>
-      <div>
-        <Router>
-          <Switch>
-            <Route path="/doctor-bio">
-              <DoctorBio />
-            </Route>
-            <Route path="/blog-post">
-              <BlogPost />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Router> 
-        <MskFooter
-          links={footerLinks}
-        />
-      </div>
-    </MskSiteWrapper>
+      <MskSiteWrapper>
+          <Router>
+            <Switch>
+              <Route path="/doctor-bio">
+                <DoctorBio />
+              </Route>
+              <Route path="/blog-post">
+                <BlogPost />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Router>
+      </MskSiteWrapper>
+      <MskFooter links={footerLinks} />
+    </div>
   );
 }
 
