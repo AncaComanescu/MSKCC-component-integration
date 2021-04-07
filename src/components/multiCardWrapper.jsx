6c0f29bf8fe66ea27dca01f9533f79f0cf229749
Rob Maurizi <maurizir@mskcc.org>
2021-04-07 13:44:27 -0400

    added card count

--- src/components/multiCardWrapper.jsx
+++ src/components/multiCardWrapper.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MskMultiCardWrapper = (props) => {
 
     /* Will wrap all child components in a single wrapper using the following structure:
-        .card.card-wrapper.has-card-type-[child_1-name].has-card-type-[child_n-name] >
+        .card.card-wrapper.has-card-type-[child_1-name].has-card-type-[child_n-name].has-[n]-cards >
             .card-inner >
                 [ child ]
         
@@ -11,13 +11,17 @@ const MskMultiCardWrapper = (props) => {
     */
 
     let classes = ['card','card-wrapper'];
+    let n = 0;
     const childrenWithProps = React.Children.map(props.children, child => {
         if (React.isValidElement(child)) {
+            n += 1;
           classes.push(`has-card-type-${child.type.name}`);
             return React.cloneElement(child);
         }
         return child;
     });
+
+    classes.push(`has-${n}-cards`);
 
     return (
         <div className={classes.join(' ')}>
