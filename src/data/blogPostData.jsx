05b55df7f4792b48c1275263af9a8c01188ad2be
AncaComanescu <80053458+AncaComanescu@users.noreply.github.com>
2021-04-09 13:58:14 -0400

    Blog post page (#11)
    * replace mock componenets in blogpost Page with Right rail componenet MWH-244
    
    * add MskActions
    
    * add componenet to display a blog post. add images used in the blog post.
    
    * add images to tiles in the right rail
    
    * enable figure component
    
    * add title to blog post

---
+++ src/data/blogPostData.jsx
@@ -0,0 +1,85 @@
+import {
+  MskTeaserCard, MskPaginationPages
+} from '@knapsack-cloud/msk-design-system/react';
+import teasercard from '../images/030519_msk-01066_rt2_1200x800.jpg'
+
+
+export const blogPostData = [
+  {
+    element: <p>Having a vaccine that protects people from getting <em>any </em>cancer is a life-saving medical breakthrough; the HPV vaccine protects against <em>many</em> cancers – those of the <a href="/cancer-care/types/cervical">cervix</a>, <a href="/cancer-care/types/anal">anus</a>, <a href="/cancer-care/types/throat">throat</a>, penis, vagina, and more. All of them can result from HPV, the human papillomavirus.
+    </p>
+  },
+  {
+    element: <p>“It’s far better to prevent a cancer than treat a cancer,” says Andrew Kung, Chair of the Department of Pediatrics at MSK Kids. According to the Centers for Disease Control and Prevention, getting vaccinated could prevent an estimated 92 percent of cancers attributed to HPV.
+    </p>
+  },
+  {
+    element: <p>“It’s far better to prevent a cancer than treat a cancer,” says <a href="/cancer-care/doctors/andrew-kung">Andrew Kung</a>, Chair of the Department of Pediatrics at <a href="/pediatrics">MSK Kids</a>. According to the Centers for Disease Control and Prevention, getting vaccinated could prevent <a href="https://www.cdc.gov/media/releases/2019/p0822-cancer-prevented-vaccine.html" target="_blank" rel="noopener">an estimated 92 percent of cancers attributed to HPV.</a>
+    </p>
+  },
+  {
+    element: <p>HPV has led to an <a href="https://www.cancernetwork.com/view/late-stage-head-and-neck-cancer-us-sees-increasing-incidence" target="_blank" rel="noopener">alarming increase</a> in advanced head and neck cancers, especially among white men. So it’s just as important for boys to be vaccinated as girls.
+    </p>
+  },
+  {
+    element: <p>And yet, the number of parents reluctant to have their child vaccinated is rising, according to <a href="https://pediatrics.aappublications.org/content/147/3/e2020026286" target="_blank" rel="noopener">a recent study published in the journal <em>Pediatrics</em></a>. Study results indicated that some parents are worried about potential side effects of the vaccine, despite the fact that 15 years after its approval by the Food and Drug Administration (FDA), the number of side effects has been low.
+    </p>
+  },
+  {
+    element: <p>It’s natural for parents to want as much information as possible about vaccines, so Dr. Kung offers these answers to key questions.
+      </p>
+  },
+  {
+    element: <h2>What is HPV and how does it cause cancer?
+      </h2>
+  },
+  {
+    element: <p>HPV is the most common sexually transmitted infection in the United States. There are more than 100 types of HPV, some of which can cause serious health problems, like cancer. There are roughly 45,000 HPV-associated cancers diagnosed each year. About 25,400 cases are in women and 19,900 are in men.
+      </p>
+  },
+  {
+    element: <p>“As a pediatric oncologist, I’m often asked if there are things a parent can do to prevent their child from getting cancer,” says Dr. Kung. “The answer is yes: There are certain things that can be done in childhood to prevent cancer later, like getting vaccinated against HPV.”
+        </p>
+  },
+  { element: <p>back to top</p> },
+  { element: <h2 class="right-rail__subtitle" id="the-hpv-vaccine-does-not-encourage-promiscuity-it-protects-children-from-developing-cancer-when-they-grow-up">The HPV vaccine does not encourage promiscuity; it protects children from developing cancer when they grow up.</h2> },
+  {
+    element: <p><a href="https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(19)30298-3/fulltext" target="_blank" rel="noopener">A major study</a> of 66 million young people worldwide found that the HPV vaccine reduced pre-cancerous cervical lesions by more than 50 percent in young women.&nbsp;
+      </p>
+  },
+  {
+    element: <p>Dr. Kung compares the HPV vaccine to the Hepatitis B vaccine, which children routinely receive. Hepatitis B can also spread through sexual activity and can contribute to <a href="/cancer-care/types/liver">liver cancer</a> in adults. “The rationale for vaccinating against HPV is the same,” he says.
+      </p>
+  },
+  {
+    element: <MskTeaserCard
+      hasHighlight={true}
+      mediaAlignment="left"
+      lowResLargeImg={teasercard}
+      largeImg={teasercard}
+      smallImg={teasercard}
+      lowResSmallImg={teasercard}
+      heading="Think You’re Too Old to Get the HPV Vaccine to Prevent Cancer? Maybe Not"
+      buttonText="Learn More"
+      href="#"
+    >
+      <p>Learn from an MSK expert what people over 26 should know about getting the HPV vaccine to protect themselves from several forms of cancer.
+      </p>
+    </MskTeaserCard>
+  },
+  { element: <p>back to top</p> },
+  { element: <h2 class="right-rail__subtitle" id="the-vaccine-has-a-proven-track-record-of-safety">The vaccine has a proven track record of safety.</h2> },
+  {
+    element: <p>“The safety requirements for a vaccine to receive FDA approval are extremely high,” says Dr. Kung. The FDA looked at safety data from <a href="https://www.cdc.gov/hpv/hcp/vaccine-safety-data.html" target="_blank" rel="noopener">tens of thousands of people</a> before approving the vaccine. Since then, the FDA has tracked results of the vaccine. As of 2018, data from more than 120 million administered doses show no evidence it causes harm, according to the <a href="https://www.cancer.gov/about-cancer/causes-prevention/risk/infectious-agents/hpv-vaccine-fact-sheet" target="_blank" rel="noopener">National Cancer Institute.</a>
+    </p>
+  },
+  { element: <p>back to top</p> },
+  {
+    element: <MskPaginationPages
+      prevPage="In the News"
+      nextUrl="#"
+      prevUrl="#"
+      nextPage="Four Things Parents Should Know about the HPV Vaccine"
+    />
+  }
+];
--- src/pages/blogPost.jsx
+++ src/pages/blogPost.jsx
@@ -1,59 +1,235 @@
 import React from "react";
 import {
-    MskLeftRail, MskBreadcrumb, BaseballCard
+  MskBreadcrumb, MskRightRail, MskActions, MskSummary, MskAccordion, MskTile, MskGrid, MskGridRow, MskList, MskFigure,//, MskBackToTop, MskNavWidget, MskButton
 } from '@knapsack-cloud/msk-design-system/react';
 
+import { blogPostData } from '../data/blogPostData'
+
+///TODO: enable for rendering componenent MskFigure
+import child_vaccine from '../images/img_6070_2_hq_ret_1200x800.jpg';
+import child_vaccine_w200 from '../images/img_6070_2_hq_ret_1200x800_w200.jpg';
+import child_vaccine_w300 from '../images/img_6070_2_hq_ret_1200x800_w300.jpg';
+import child_vaccine_w400 from '../images/img_6070_2_hq_ret_1200x800_w400.jpg';
+import child_vaccine_w500 from '../images/img_6070_2_hq_ret_1200x800_w500.jpg';
+import child_vaccine_w600 from '../images/img_6070_2_hq_ret_1200x800_w600.jpg';
+import child_vaccine_w700 from '../images/img_6070_2_hq_ret_1200x800_w700.jpg';
+import child_vaccine_w800 from '../images/img_6070_2_hq_ret_1200x800_w800.jpg';
+import hpv2_3x2 from '../images/hpv2_3x2.jpeg';
+import hpv2_3x2_w200 from '../images/hpv2_3x2_w200.jpeg';
+import hpv2_3x2_w300 from '../images/hpv2_3x2_w300.jpeg';
+import hpv2_3x2_w400 from '../images/hpv2_3x2_w400.jpeg';
+import hpv2_3x2_w500 from '../images/hpv2_3x2_w500.jpeg';
+import hpv2_3x2_w600 from '../images/hpv2_3x2_w600.jpeg';
+import hpv2_3x2_w700 from '../images/hpv2_3x2_w700.jpeg';
+import hpv2_3x2_w800 from '../images/hpv2_3x2_w800.jpeg';
+
 function BlogPost(props) {
-    const pageIntro = (<>
-        This is the pageIntro
-        <MskBreadcrumb
-            breadcrumb={[
-                { url: 'https://example.com', text: 'Item 1' },
-                { url: 'https://example.com', text: 'Item 2' },
-                { url: 'https://example.com', text: 'Item 3' },
-            ]}
-        />
-    </>);
-    const leftRail = (<p>
-        This is the left rail.
-    </p>);
-    const pageOutro = (<p>
-        This is the page outro.
-    </p>);
-    const imgUrl300='https://msk-design-system.herokuapp.com/images/sample-virus-image-3x2-300w';
-    const imgUrl800="https://msk-design-system.herokuapp.com/images/sample-virus-image-3x2-800w";
-    const imgUrlPlaceholder ="https://msk-design-system.herokuapp.com/images/placeholder/3x2.svg";
-    
-    const mainContent = (
-        <>
-            <BaseballCard
-                img={{
-                    src: `${imgUrl300}.webp`,
-                    // srcset: `${imgUrl300}.jpg 300w, ${imgUrl800}.jpg 800w`,  
-                    webpSrcset:`${imgUrl300}.webp  300w, ${imgUrl800}.webp  800w`,   
-                    sizes: '250px',
-                    alt: 'Virus Image',
-                    lowResSrc:`${imgUrlPlaceholder}`,
-                    lowResSrcset: `${imgUrlPlaceholder}`,
-                }}
-                hasDivider={false}
-                overlineText="In The Clinic"
-                heading="Heading that can be 1-2 lines if necessary for content"
-                subheading="Cotent varies by content type. Byline or date for blogs."
-                bodyCopy="Body copy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                href="#"
-            />
-        </>
-    );
-    return (
-        <MskLeftRail 
-            lang="en"
-            classes={[]}
-            pageIntro={pageIntro}
-            leftRail={leftRail}
-            pageOutro={pageOutro}
-            mainContent={mainContent}
-        />
-    );
+
+  const mainContent = (
+    <><h1>
+      <span >This Vaccine Can Protect Your Child Against Cancer:  Get the Facts about the HPV Vaccine</span>
+    </h1>
+      <MskActions
+        shareUrl="https://www.mskcc.org"
+        printHref="https://www.mskcc.org/test"
+        languages={[
+          { name: 'english', href: '#', langCode: 'en' },
+        ]}///workaround to render the component
+
+        shareTitle="Share Title"
+        shareSource="https://www.mskcc.org"
+      />
+      <MskFigure
+        personProfile={false}
+        img={{
+          src: { child_vaccine },
+          srcset:
+            `${child_vaccine_w200} 200w, ${child_vaccine_w300} 300w, ${child_vaccine_w400} 400w,${child_vaccine_w500} 500w,${child_vaccine_w600} 600w,${child_vaccine_w700} 700w, ${child_vaccine_w800} 800w`,
+          sizes: '(min-width: 1250px) 780px, (min-width: 950px) 64vw, calc(100vw - 30px)',
+          alt: 'Young girl',
+        }}
+        borderless={false}
+        align="center"
+        bodyText="Gregory Page, a dentist in Harlem in New York City, came to Memorial Sloan Kettering for a less invasive treatment for his prostate cancer. After surgery with MSK's Jonathan Coleman, he's back to living life as normal."
+      />
+      <div class="abstract-summary__title">Summary (title can e part of summary component)</div>
+      <MskSummary >
+        <p>
+          The human papillomavirus (HPV) vaccine protects against not only HPV but also certain types of cancer. Yet, a new study has found more parents are reluctant to have their child vaccinated. <a href="/cancer-care/doctors/andrew-kung">Andrew Kung</a>, Chair of the Department of Pediatrics at <a href="/pediatrics">MSK Kids</a>, explains why it’s so important to get this vaccine.
+        </p>
+      </MskSummary>
+      {blogPostData.map((element, index) => {
+        return element.element;
+      })}
+    </>
+  );
+
+  const pageIntro = <>
+    <MskBreadcrumb
+      breadcrumb={[
+        { url: "https://www.mskcc.org/news", text: 'News & Information' },
+        { url: "https://www.mskcc.org/news/in-the-news", text: 'In the News' }
+      ]}
+    />
+  </>
+  const rightRail = (<>
+
+    <MskGrid types={{ small: '100', medium: '100', large: '100' }}>
+      <MskGridRow>
+        <div>placeholder for MskNavWidget </div>
+        {/* TODO: enable for rendering componenent MskNavWidget
+    <MskNavWidget
+        hasBackToTop
+        links={[
+          { url: '#heading-one', text: 'link to heading one', isActive: false },
+          { url: '#heading-two', text: 'link to heading two', isActive: true },
+          {
+            url: '#heading-three',
+            text: 'link to heading three',
+            isActive: false,
+          },
+        ]}
+      /> */}
+      </MskGridRow>
+      <MskGridRow>
+        <MskAccordion
+          headlineSize="card-title"
+          isMinimal={false}
+          isReadMore={false}
+          isInverted={true}
+          state={true}
+          headline="Related news"
+          classes={[]}
+        >
+          <MskTile
+            img={{
+              src: { hpv2_3x2 },
+              srcset: `${hpv2_3x2_w800} 800w,${hpv2_3x2_w700} 700w,  ${hpv2_3x2_w600}600w,${hpv2_3x2_w500} 500w,${hpv2_3x2_w400} 400w, ${hpv2_3x2_w300} 300w,${hpv2_3x2_w200} 200w`,
+              sizes: "(min-width: 1250px) 366px, (min-width: 768px) 30vw, calc(100vw - 60px)",
+              alt: 'Group of racially diverse boys and girls lying on grass and conversing.'
+            }}
+            title="Four Things Parents Should Know about the HPV Vaccine "
+            titleUrl="/news/four-things-parents-should-know-about-hpv-vaccine"
+          >
+          </MskTile>
+          <MskTile
+            img={{
+              src: "https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/115939/3x2/hpv.jpeg",
+              srcset: `https://www.mskcc.org/sites/default/files/styles/width_800_3x1/public/node/115939/3x2/hpv.jpeg 800w
+          ,https://www.mskcc.org/sites/default/files/styles/width_700_3x1/public/node/115939/3x2/hpv.jpeg 700w,
+          https://www.mskcc.org/sites/default/files/styles/width_600_3x1/public/node/115939/3x2/hpv.jpeg 600w,
+          https://www.mskcc.org/sites/default/files/styles/width_500_3x1/public/node/115939/3x2/hpv.jpeg 500w,
+          https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/115939/3x2/hpv.jpeg 400w,
+          https://www.mskcc.org/sites/default/files/styles/width_300_3x1/public/node/115939/3x2/hpv.jpeg 300w,
+          https://www.mskcc.org/sites/default/files/styles/width_200_3x1/public/node/115939/3x2/hpv.jpeg 200w`,
+              sizes: "(min-width: 1250px) 366px, (min-width: 768px) 30vw, calc(100vw - 60px)",
+              alt: "3D illustration of human papilloma virus, depicted by floating spheres "
+            }}
+            title="MSK Issues Joint Statement Urging Use of HPV Vaccine to Prevent Cancers"
+            titleUrl="/news/msk-issues-joint-statement-urging-use-hpv-vaccine-prevent-cancers"
+          >
+          </MskTile>
+          <MskTile
+            img={{
+              src: "https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/28092/image/singh-bhuvanesh.jpg",
+              srcset: `https://www.mskcc.org/sites/default/files/styles/width_800_3x1/public/node/28092/image/singh-bhuvanesh.jpg 800w,
+ https://www.mskcc.org/sites/default/files/styles/width_700_3x1/public/node/28092/image/singh-bhuvanesh.jpg 700w,
+ https://www.mskcc.org/sites/default/files/styles/width_600_3x1/public/node/28092/image/singh-bhuvanesh.jpg 600w
+ ,https://www.mskcc.org/sites/default/files/styles/width_500_3x1/public/node/28092/image/singh-bhuvanesh.jpg 500w,
+ https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/28092/image/singh-bhuvanesh.jpg 400w,
+ https://www.mskcc.org/sites/default/files/styles/width_300_3x1/public/node/28092/image/singh-bhuvanesh.jpg 300w,
+ https://www.mskcc.org/sites/default/files/styles/width_200_3x1/public/node/28092/image/singh-bhuvanesh.jpg 200w`,
+              sizes: "(min-width: 1250px) 366px, (min-width: 768px) 30vw, calc(100vw - 60px)",
+              alt: "Pictured: Bhuvanesh Singh"
+
+
+            }}
+            title="HPV and Cancers of the Head and Neck: Common Questions and Answers"
+            titleUrl="/news/hpv-and-head-and-neck-common-questions-and-answers"
+          >
+          </MskTile>
+          <MskTile
+            img={{
+              src: "https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg",
+              srcset: `https://www.mskcc.org/sites/default/files/styles/width_800_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg 800w,
+          https://www.mskcc.org/sites/default/files/styles/width_700_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg 700w,
+          https://www.mskcc.org/sites/default/files/styles/width_600_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg 600w,
+          https://www.mskcc.org/sites/default/files/styles/width_500_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg 500w,
+          https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg 400w,
+          https://www.mskcc.org/sites/default/files/styles/width_300_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg 300w,
+          https://www.mskcc.org/sites/default/files/styles/width_200_3x1/public/node/164530/3x2/istock-905899402_1200x800.jpg 200w`,
+              sizes: "(min-width: 1250px) 366px, (min-width: 768px) 30vw, calc(100vw - 60px)",
+              alt: "A nurse gives a girl a vaccination."
+
+            }}
+            title="HPV Vaccine and Cancer Risk: Frequently Asked Questions"
+            titleUrl="/news/hpv-vaccine-and-cancer-risk-frequently-asked-questions"
+          >
+          </MskTile>
+
+          <MskTile
+            img={{
+
+              src: "https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/19787/image/brown-carol.jpg",
+              srcset: `https://www.mskcc.org/sites/default/files/styles/width_800_3x1/public/node/19787/image/brown-carol.jpg 800w,
+https://www.mskcc.org/sites/default/files/styles/width_700_3x1/public/node/19787/image/brown-carol.jpg 700w,
+https://www.mskcc.org/sites/default/files/styles/width_600_3x1/public/node/19787/image/brown-carol.jpg 600w,
+https://www.mskcc.org/sites/default/files/styles/width_500_3x1/public/node/19787/image/brown-carol.jpg 500w,
+https://www.mskcc.org/sites/default/files/styles/width_400_3x1/public/node/19787/image/brown-carol.jpg 400w,
+https://www.mskcc.org/sites/default/files/styles/width_300_3x1/public/node/19787/image/brown-carol.jpg 300w,
+https://www.mskcc.org/sites/default/files/styles/width_200_3x1/public/node/19787/image/brown-carol.jpg 200w`,
+              sizes: "(min-width: 1250px) 366px, (min-width: 768px) 30vw, calc(100vw - 60px)",
+              alt: "Pictured: Carol Brown"
+
+
+            }}
+            title="Gynecologic Surgeon Carol Brown Discusses Importance of HPV Vaccine"
+            titleUrl="/news/gynecologic-surgeon-carol-brown-discusses-importance-hpv-vaccine"
+          >
+          </MskTile>
+
+
+
+        </MskAccordion>
+      </MskGridRow>
+      <MskGridRow>
+        <MskAccordion
+          headlineSize="card-title"
+          isMinimal={false}
+          isReadMore={false}
+          isInverted={false}
+          state={true}
+          headline="Related People"
+          classes={[]}
+        >
+          <MskGrid types={{ small: '50:50', medium: '66:33', large: '66:33' }}>
+            <MskGridRow>
+              <div className="demo-box">Andrew Kung
+Chair, Department of Pediatrics</div>
+            </MskGridRow>
+          </MskGrid>
+        </MskAccordion>
+
+      </MskGridRow>
+      <MskGridRow>
+        <MskTile
+          title="Stay Informed."
+          buttonText="Newsletter Sign Up"
+        >
+          <div>Get the latest news and updates on MSK’s cancer care and research breakthroughs sent straight to your inbox with our e-newsletters.</div>
+        </MskTile>
+      </MskGridRow>       </MskGrid>
+  </>)
+  return (
+    <MskRightRail
+      lang="en"
+      classes={[]}
+      pageLevelLayout
+      pageIntro={pageIntro}
+      rightRail={rightRail}
+      mainContent={mainContent}
+    />
+
+  );
 }
 export default BlogPost;
